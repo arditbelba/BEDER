0ca3486687f6f312642ab89430e3f1e17fa96c3c
arditbelba <62603290+arditbelba@users.noreply.github.com>
2022-12-16 18:58:59 +0100

    Add files via upload

---
+++ StudentApp1/Controllers/StudentC.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StudentApp.Models;
+
+namespace StudentApp.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class StudentC : ControllerBase
+{
+ 
+    static List<Student> Students = new()
+    {
+       new Student
+       {
+           Id = 1,
+           DateCreated = DateTime.Today,
+           DateUpdted = DateTime.Today,
+           DateOfBirth = DateTime.Today.AddYears(-70),
+           FirstName = "John",
+           LastName = "Cena",
+           GraduationYear = 1988,
+       },
+       new Student
+       {
+           Id = 2,
+           DateCreated = DateTime.Today,
+           DateUpdted = DateTime.Today,
+           DateOfBirth = DateTime.Today.AddYears(-50),
+           FirstName = "Rey",
+           LastName = "Misteryo",
+           GraduationYear = 1990,
+       }
+    };
+
+
+    [HttpPost]
+    public IActionResult Post([FromBody] Student student)
+    {
+        student.Id = Students.Count + 1;
+        student.DateCreated = DateTime.Today;
+
+        Students.Add(student);
+        return Ok("Student added");
+    }
+
+    [HttpPut]
+    public IActionResult Put([FromBody] Student student)
+    {
+        if (student.Id > Students.Count)
+            return NotFound("Student not found!");
+
+        if (student.DateUpdted != student.DateCreated)
+            return NotFound("Student can't be updated");
+
+
+        student.DateUpdted = DateTime.Today;
+        Students[student.Id - 1] = student;
+        return Ok("Student updated");
+    }
+}
